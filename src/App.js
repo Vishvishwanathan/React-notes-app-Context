14a02095d0db5e29c6fdc9a9a2e9343775941f41
Vishvishwanathan <vishvishva204@gmail.com>
2023-12-18 23:11:46 +0530

    First commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,44 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
-
+import { Routes,Route,} from 'react-router-dom';
+import { useAuthentication } from './Contexts/Authentication.context';
+import Navbar from './Companents/Navbar';
+import Home from './Pages/Home';
+import Notes from './Pages/Notes';
+import Task from './Pages/Task';
+import Login from './Pages/Login';
+import Signup from './Pages/Signup';
 function App() {
+ 
+  const { loggedIn} = useAuthentication();
+  function renderRoutes(flag=false){
+    if(flag) {
+      return(
+        <React.Fragment>
+        <Route path="/Home" Component={Home} />
+        <Route path="/Notes"Component={Notes}/>
+        <Route path="/Task" Component={Task} />
+        </React.Fragment>
+      )
+    }
+    else {
+     return(
+      <React.Fragment>
+      <Route path="/" Component={Login}/>
+     <Route path="Sign up" Component={Signup}/>
+   </React.Fragment>
+     )
+    }
+    
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div className='app-container '>
+   <Navbar />
+     <Routes>
+     {renderRoutes(loggedIn)}
+    </Routes>
+      </div>
+     
   );
 }
 
---
+++ src/Pages/Login.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+export default function Login (){
+    return (
+        <div className="cards">
+          <div className="col-md-12">
+            <div className="cards-body">
+            <h1>LOGIN</h1>
+            </div>
+          </div>
+        </div>
+    )
+}
